1744869bb7582d00fadddf9c9a64609c45d352c4
Ayooluwa Adeleke <51849584+PolyMath-Coder@users.noreply.github.com>
2023-07-04 13:00:04 +0100

    add: needful dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      username: 'root',
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      entities: ['dist/**/*.entity.js'],
+      migrations: ['dist/migrations/*.js'],
+      migrationsRun: true,
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
