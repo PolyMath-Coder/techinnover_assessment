62de68b30cb10f75e1b1d76814c61ba1f39c4fe7
Ayooluwa Adeleke <51849584+PolyMath-Coder@users.noreply.github.com>
2023-07-05 02:14:31 +0100

    chore: set up entities for medication & drone

--- src/shared/entities/drone.entity.ts
+++ src/shared/entities/drone.entity.ts
@@ -1,1 +1,28 @@
-export class Drone {}
+import { Column, Entity, JoinColumn, OneToMany } from 'typeorm';
+
+import { BaseEntity } from './base.entity';
+import { MedicationEntity } from './medication.entity';
+import { stateEnum } from '../enums';
+
+export class DroneEntity extends BaseEntity {
+  @Column()
+  serialNumber: number;
+
+  @Column()
+  model: string;
+
+  @Column()
+  weightLimit: number;
+
+  @Column()
+  batteryCapacity: number;
+
+  @Column({ type: 'enum', enum: stateEnum, default: stateEnum.IDLE })
+  state: string;
+
+  @Column()
+  @OneToMany(() => MedicationEntity, (medication) => medication.drone, {
+    eager: true,
+  })
+  medications: MedicationEntity[];
+}
--- src/shared/entities/medication.entity.ts
+++ src/shared/entities/medication.entity.ts
@@ -1,1 +1,20 @@
-export class Medication {}
+import { Column, ManyToOne } from 'typeorm';
+import { BaseEntity } from './base.entity';
+import { DroneEntity } from './drone.entity';
+
+export class MedicationEntity extends BaseEntity {
+  @Column()
+  name: string;
+
+  @Column()
+  weight: number;
+
+  @Column()
+  code: string;
+
+  @Column()
+  image?: string;
+
+  @ManyToOne(() => DroneEntity, (drone) => drone.medications)
+  drone: DroneEntity;
+}
